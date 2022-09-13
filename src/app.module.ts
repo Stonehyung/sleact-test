9a067c1eb32f7729ea69b646b6fc6b47864ae39c
Stonehyung <qkdgkr1256@gmail.com>
2022-09-13 20:06:34 +0900

    feat/middleware설정완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { ConfigurableModuleBuilder, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MiddlewareBuilder } from '@nestjs/core';
+import { consumers } from 'stream';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
 
 
 
